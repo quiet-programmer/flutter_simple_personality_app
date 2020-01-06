c3c5aead56d0c3f52525971c394d8563e812c42b
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-01-06 12:03:12 +0100

    added all necessary files

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -22,6 +22,7 @@ class Quiz extends StatelessWidget {
         ...(questions[questionIndex]['answers'] as List<Map<String, Object>>).map((answer) {
           return Answer(()=> answerQuestion(answer['score']), answer['text']);
         }).toList(),
+        
       ],
     );
   }
